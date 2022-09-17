2da75eeb6ae7aafe771ccd92c0e0670d3fed26ae
Aly-Ant <90076069+Aly-Ant0@users.noreply.github.com>
2022-09-17 04:41:33 -0300

    h024 - change paths

--- source/Paths.hx
+++ source/Paths.hx
@@ -13,13 +13,12 @@ import openfl.utils.AssetType;
 import openfl.utils.Assets as OpenFlAssets;
 import lime.utils.Assets;
 import flixel.FlxSprite;
-#if sys
+#if MODS_ALLOWED
 import sys.io.File;
 import sys.FileSystem;
 #end
 import flixel.graphics.FlxGraphic;
 import openfl.display.BitmapData;
-import haxe.Json;
 
 import flash.media.Sound;
 
@@ -27,8 +26,9 @@ using StringTools;
 
 class Paths
 {
+	public static var menuMusic:String = '';
 	inline public static var SOUND_EXT = #if web "mp3" #else "ogg" #end;
-	inline public static var VIDEO_EXT = "mp4";
+	inline public static var VIDEO_EXT = "";
 
 	#if MODS_ALLOWED
 	public static var ignoreModFolders:Array<String> = [
@@ -66,7 +66,7 @@ class Paths
 		// clear non local assets in the tracked assets list
 		for (key in currentTrackedAssets.keys()) {
 			// if it is not currently contained within the used local assets
-			if (!localTrackedAssets.contains(key)
+			if (!localTrackedAssets.contains(key) 
 				&& !dumpExclusions.contains(key)) {
 				// get rid of it
 				var obj = currentTrackedAssets.get(key);
@@ -100,13 +100,13 @@ class Paths
 
 		// clear all sounds that are cached
 		for (key in currentTrackedSounds.keys()) {
-			if (!localTrackedAssets.contains(key)
+			if (!localTrackedAssets.contains(key) 
 			&& !dumpExclusions.contains(key) && key != null) {
 				//trace('test: ' + dumpExclusions, key);
 				Assets.cache.clear(key);
 				currentTrackedSounds.remove(key);
 			}
-		}
+		}	
 		// flags everything to be cleared out next unused memory clear
 		localTrackedAssets = [];
 		openfl.Assets.cache.clear("songs");
@@ -187,9 +187,12 @@ class Paths
 	}
 	inline static public function lua(key:String, ?library:String)
 	{
-		return getPath('$key.lua', TEXT, library);
+		return Main.path + getPath('$key.lua', TEXT, library);
 	}
-
+	inline static public function luaAsset(key:String, ?library:String)
+        {
+		return getPath('$key.lua', TEXT, library);
+        }
 	static public function video(key:String)
 	{
 		#if MODS_ALLOWED
@@ -198,7 +201,7 @@ class Paths
 			return file;
 		}
 		#end
-		return SUtil.getPath() + 'assets/videos/$key.$VIDEO_EXT';
+		return 'assets/videos/$key';
 	}
 
 	static public function sound(key:String, ?library:String):Sound
@@ -206,7 +209,7 @@ class Paths
 		var sound:Sound = returnSound('sounds', key, library);
 		return sound;
 	}
-
+	
 	inline static public function soundRandom(key:String, min:Int, max:Int, ?library:String)
 	{
 		return sound(key + FlxG.random.int(min, max), library);
@@ -237,6 +240,93 @@ class Paths
 		// streamlined the assets process more
 		var returnAsset:FlxGraphic = returnGraphic(key, library);
 		return returnAsset;
+	}
+
+	// im fucking dumbass
+	// -aly ant
+	inline static public function h024Menu(key:String, menuLibrary:String)
+	{
+		var shit:String = '';
+
+		if (menuLibrary == 'CREDITS'){ // credits mneu
+			shit = 'hotline/menu/credits';
+		}
+
+		if (menuLibrary == 'NONE'){ // none
+			shit = 'hotline/menu/';
+		}
+
+		if (menuLibrary == 'JUKE'){ // jukebox menu
+			shit = 'hotline/menu/jukebox';
+		}
+
+		if ( menuLibrary == 'SKIN'){ // skins menu
+			shit = 'hotline/menu/skins';
+		}
+
+		if (menuLibrary == 'OPTION'){ // options menu
+			shit = 'hotline/menu/options';
+		}
+
+		if (menuLibrary == 'EXTRA'){ // extras menu
+			shit = 'hotline/menu/extras';
+		}
+
+		if (menuLibrary == 'FREEPLAY'){
+			shit = 'hotline/menu/freeplay';
+		}
+		// freeplay menu gopicoyeayeagopicooh
+		/*else {
+			shit = 'ah, enfia no teu cu ent';
+		}*/
+		return image('$shit/$key', 'preload');
+	}
+	inline static public function h024MenuAnim(key:String, menuLibrary:String)
+	{
+		var shit:String = '';
+		if (menuLibrary == 'CREDITS'){ // credits mneu
+			shit = 'hotline/menu/credits';
+		}
+
+		if (menuLibrary == 'NONE'){ // none
+			shit = 'hotline/menu/';
+		}
+
+		if (menuLibrary == 'JUKE'){ // jukebox menu
+			shit = 'hotline/menu/jukebox';
+		}
+
+		if ( menuLibrary == 'SKIN'){ // skins menu
+			shit = 'hotline/menu/skins';
+		}
+
+		if (menuLibrary == 'OPTION'){ // options menu
+			shit = 'hotline/menu/options';
+		}
+
+		if (menuLibrary == 'EXTRA'){ // extras menu
+			shit = 'hotline/menu/extras';
+		}
+
+		if (menuLibrary == 'FREEPLAY'){
+			shit = 'hotline/menu/freeplay';
+		}
+		// freeplay menu gopicoyeayeagopicooh
+		/*else {
+			shit = 'ah, enfia no teu cu ent';
+		}*/
+		return getSparrowAtlas('$shit/$key', 'preload');
+	}
+
+	// for main menu text that show the music that are playing
+	inline static public function h024Music(key:String, library:String, txt:String)
+	{
+		menuMusic = txt;
+		if (txt == null)
+		{
+			menuMusic = 'I FORGOR LMAO';
+		}
+		return music('$key');
 	}
 
 	static public function getTextFromFile(key:String, ?ignoreMods:Bool = false):String
@@ -283,7 +373,7 @@ class Paths
 			return true;
 		}
 		#end
-
+		
 		if(OpenFlAssets.exists(getPath(key, type))) {
 			return true;
 		}
@@ -323,6 +413,15 @@ class Paths
 
 	inline static public function formatToSongPath(path:String) {
 		return path.toLowerCase().replace(' ', '-');
+	}
+
+	inline static public function h024Song(key:String, shit:String) // for jukebox menu
+	{
+		menuMusic = shit;
+		if (shit == null){
+			menuMusic = 'I FORGOR LMAO PART 2 LETS GOOOO';
+		}
+		return inst('$key');
 	}
 
 	// completely rewritten asset loading? fuck!
@@ -370,29 +469,21 @@ class Paths
 		}
 		#end
 		// I hate this so god damn much
-		var gottenPath:String = SUtil.getPath() + getPath('$path/$key.$SOUND_EXT', SOUND, library);
+		var gottenPath:String = SUtil.getPath() + getPath('$path/$key.$SOUND_EXT', SOUND, library);	
 		gottenPath = gottenPath.substring(gottenPath.indexOf(':') + 1, gottenPath.length);
 		// trace(gottenPath);
 		if(!currentTrackedSounds.exists(gottenPath))
-		#if MODS_ALLOWED
 			currentTrackedSounds.set(gottenPath, Sound.fromFile(gottenPath));
-		#else
-		{
-			var folder:String = '';
-			if(path == 'songs') folder = 'songs:';
-
-			currentTrackedSounds.set(gottenPath, OpenFlAssets.getSound(folder + getPath('$path/$key.$SOUND_EXT', SOUND, library)));
-		}
-		#end
+		
 		localTrackedAssets.push(gottenPath);
 		return currentTrackedSounds.get(gottenPath);
 	}
-
+	
 	#if MODS_ALLOWED
 	inline static public function mods(key:String = '') {
 		return SUtil.getPath() + 'mods/' + key;
 	}
-
+	
 	inline static public function modsFont(key:String) {
 		return modFolders('fonts/' + key);
 	}
@@ -402,7 +493,7 @@ class Paths
 	}
 
 	inline static public function modsVideo(key:String) {
-		return modFolders('videos/' + key + '.' + VIDEO_EXT);
+		return modFolders('videos/' + key +);
 	}
 
 	inline static public function modsSounds(path:String, key:String) {
@@ -442,51 +533,8 @@ class Paths
 				return fileToCheck;
 			}
 		}
-
-		for(mod in getGlobalMods()){
-			var fileToCheck:String = mods(mod + '/' + key);
-			if(FileSystem.exists(fileToCheck))
-				return fileToCheck;
-
-		}
 		return SUtil.getPath() + 'mods/' + key;
 	}
-
-	public static var globalMods:Array<String> = [];
-
-	static public function getGlobalMods()
-		return globalMods;
-
-	static public function pushGlobalMods(){ // prob a better way to do this but idc
-		globalMods = [];
-		if (FileSystem.exists(SUtil.getPath() + "modsList.txt"))
-		{
-			var list:Array<String> = CoolUtil.listFromString(File.getContent(SUtil.getPath() + "modsList.txt"));
-			for (i in list)
-			{
-				var dat = i.split("|");
-				if (dat[1] == "1")
-				{
-					var folder = dat[0];
-					var path = Paths.mods(folder + '/pack.json');
-					if(FileSystem.exists(path)) {
-						try{
-							var rawJson:String = File.getContent(path);
-							if(rawJson != null && rawJson.length > 0) {
-								var stuff:Dynamic = Json.parse(rawJson);
-								var global:Bool = Reflect.getProperty(stuff, "runsGlobally");
-								if(global)globalMods.push(dat[0]);
-							}
-						}catch(e:Dynamic){
-							trace(e);
-						}
-					}
-				}
-			}
-		}
-		return globalMods;
-	}
-
 	static public function getModDirectories():Array<String> {
 		var list:Array<String> = [];
 		var modsFolder:String = mods();
